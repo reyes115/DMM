9c564c003e240cfa7b3d298af7071c5d1fdf7118
Antonio Martínez <jesus07antonio03@gmail.com>
2021-11-28 00:01:55 -0600

    Google Maps V5

--- google.js
+++ google.js
@@ -13,8 +13,8 @@ const GoogleStrategy=require('passport-google-oauth20');
 
 /*google configuracion*/
 passport.use(new GoogleStrategy({
-    clientID:'803716388072-u77mbo82e0rr3gvvj3vt6gta6ohrklf0.apps.googleusercontent.com',
-    clientSecret:'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',
+    clientID:'726237624469-doda1dn34958lvfib7290ohvbta8h5ek.apps.googleusercontent.com',
+    clientSecret:'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',
     callbackURL:'http://localhost:3000/google/callback'
 },
 (accessToken,refreshToken,profile,cb)=>{
